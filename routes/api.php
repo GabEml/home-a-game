0e943fd5a093fbaba52dd1782dd872b02adfab7c
valentinbarriol <valentin@pbsformation.fr>
2021-11-17 14:42:16 +0100

    beginapi

--- routes/api.php
+++ routes/api.php
@@ -82,7 +82,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('goodies/{goodie}', [GoodieController::class, 'update']);
     Route::delete('goodies/{goodie}', [GoodieController::class, 'destroy']);
 
-    Route::middleware('cors')->prefix('api')->group(function () {
-        Route::get('/utilisateurs/utilisateurs', [UserController::class, 'indexUsers']);
-    });
+    // Route::middleware('cors')->prefix('api')->group(function () {
+    //     Route::get('/utilisateurs/utilisateurs', [UserController::class, 'indexUsers']);
+    // });
+});
+
+Route::middleware('cors')->prefix('api')->group(function () {
+    Route::get('/utilisateurs/utilisateurs', [UserController::class, 'indexUsers']);
 });
