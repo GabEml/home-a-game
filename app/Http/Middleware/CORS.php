28c52c787923821c2588f93943f0d1adeafb2e31
valentinbarriol <valentin@pbsformation.fr>
2021-11-17 14:18:33 +0100

    beginapi

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -3,21 +3,21 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
-class CORS
+class Cors
 {
-  /**
-   * Handle an incoming request.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \Closure  $next
-   * @return mixed
-   */
-  public function handle($request, Closure $next)
-  {
-    $response = $next($request)
-      ->header('Access-Control-Allow-Origin', '*')
-      ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-      ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
-  }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    }
 }
