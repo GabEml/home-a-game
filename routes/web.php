35f262573ccb0a2c31e63f8dd2ad2632fba8cffa
Vesval <clara.vesval@ynov.com>
2022-01-12 11:51:03 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -56,66 +56,6 @@ Route::get('classement/OTR', [RankingController::class, 'rankingOTR'])->name('ra
 Route::get('classement/OTR/{sessiongame}', [RankingController::class, 'rankingOTRPrevious'])->name('rankingOTR.previous');
 Route::get('classement/{sessiongame}', [RankingController::class, 'rankingPrevious'])->name('ranking.previous');
 
-<<<<<<< HEAD
-Route::get('inscription_sessions', [SessiongameUserController::class, 'create'])->middleware('auth','verified')->name('sessiongameusers.create');
-Route::post('inscription_sessions',[SessiongameUserController::class, 'store'])->middleware('auth','verified')->name('sessiongameusers.store');
-Route::post('inscription_sessions/paiement', [SessiongameUserController::class, 'storePayment'])->middleware('auth','verified')->name('sessiongameusers.storePayment');
-
-Route::get('sessions',[SessiongameController::class, 'index'])->middleware('auth','verified')->name('sessiongames.index');
-Route::get('sessions/creer',[SessiongameController::class, 'create'])->middleware('auth','verified')->name('sessiongames.create');
-Route::post('sessions/',[SessiongameController::class, 'store'])->middleware('auth','verified')->name('sessiongames.store');
-Route::get('sessions/{sessiongame}',[SessiongameController::class, 'show'])->middleware('auth','verified')->name('sessiongames.show');
-Route::get('sessions/{sessiongame}/editer',[SessiongameController::class, 'edit'])->middleware('auth','verified')->name('sessiongames.edit');
-Route::put('sessions/{sessiongame}',[SessiongameController::class, 'update'])->middleware('auth','verified')->name('sessiongames.update');
-Route::delete('sessions/{sessiongame}',[SessiongameController::class, 'destroy'])->middleware('auth','verified')->name('sessiongames.destroy');
-
-
-Route::get('sessions/{sessiongame}/defis/creer',[ChallengeController::class, 'create'])->middleware('auth','verified')->name('sessiongames.challenges.create');
-Route::post('sessions/{sessiongame}/defis',[ChallengeController::class, 'store'])->middleware('auth','verified')->name('sessiongames.challenges.store');
-Route::get('defis/{challenge}',[ChallengeController::class, 'show'])->middleware('auth','verified')->name('challenges.show');
-Route::get('defis/{challenge}/editer',[ChallengeController::class, 'edit'])->middleware('auth','verified')->name('challenges.edit');
-Route::put('defis/{challenge}',[ChallengeController::class, 'update'])->middleware('auth','verified')->name('challenges.update');
-Route::delete('defis/{challenge}',[ChallengeController::class, 'destroy'])->middleware('auth','verified')->name('challenges.destroy');
-
-Route::delete('images/{image}',[ChallengeController::class, 'destroyImage'])->middleware('auth','verified')->name('images.destroy');
-
-Route::post('defis/{challenge}/posts',[PostController::class, 'store'])->middleware('auth','verified')->name('challenges.posts.store');
-Route::delete('posts/{post}',[PostController::class, 'destroy'])->middleware('auth','verified')->name('posts.destroy');
-Route::get('posts/valides',[PostController::class, 'indexValidated'])->middleware('auth','verified')->name('posts.indexValidated');
-Route::get('posts/en-attente',[PostController::class, 'indexPending'])->middleware('auth','verified')->name('posts.indexPending');
-Route::put('posts/{post}',[PostController::class, 'update'])->middleware('auth','verified')->name('posts.update');
-Route::get('posts/en-attente/searchPending',[PostController::class, 'searchPending'])->middleware('auth','verified')->name('posts.searchPending');
-Route::get('posts/en-attente/searchValidated',[PostController::class, 'searchValidated'])->middleware('auth','verified')->name('posts.searchValidated');
-
-
-Route::get('utilisateurs/utilisateurs',[UserController::class, 'indexUsers'])->middleware('auth','verified')->name('users.indexUsers');
-Route::get('utilisateurs/admin-defis',[UserController::class, 'indexAdminChallenge'])->middleware('auth','verified')->name('users.indexAdminChallenge');
-Route::get('utilisateurs/super-admin',[UserController::class, 'indexSuperAdmin'])->middleware('auth','verified')->name('users.indexSuperAdmin');
-Route::get('utilisateurs/creer',[UserController::class, 'create'])->middleware('auth','verified')->name('users.create');
-Route::post('utilisateurs/nouveau',[UserController::class, 'storeNewUser'])->middleware('auth','verified')->name('users.storeNewUser');
-Route::post('utilisateurs',[UserController::class, 'store'])->middleware('auth','verified')->name('users.store');
-Route::get('utilisateurs/{user}/editer',[UserController::class, 'edit'])->middleware('auth','verified')->name('users.edit');
-Route::put('utilisateurs/{user}',[UserController::class, 'update'])->middleware('auth','verified')->name('users.update');
-Route::put('utilisateurs/{user}/role',[UserController::class, 'updateRole'])->middleware('auth','verified')->name('users.updateRole');
-Route::post('utilisateurs/{user}/sessiongames',[UserController::class, 'storeSessiongame'])->middleware('auth','verified')->name('users.storeSessiongame');
-Route::delete('utilisateurs/{sessiongameUser}/sessions',[UserController::class, 'deleteSessiongameUser'])->middleware('auth','verified')->name('users.destroySessiongameUser');
-Route::delete('utilisateurs/{user}',[UserController::class, 'destroy'])->middleware('auth','verified')->name('users.destroy');
-Route::get('utilisateurs/utilisateurs/search',[UserController::class, 'search'])->middleware('auth','verified')->name('users.search');
-
-Route::get('/utilisateur/profil', [UserProfileController::class, 'show'])->middleware('auth')->name('profile');
-Route::get('paiement/{id}/{sessiongames?}', [PaymentController::class, 'show'])->middleware('auth','verified')->name('payment');
-Route::post('webhook', [WebhookController::class, 'show'])->middleware('auth','verified')->name('webhook');
-
-Route::get('/tirage_gagnant/create',[RankingController::class, 'create'])->middleware('auth','verified')->name('ranking.create');
-Route::post('/tirage_gagnant',[RankingController::class, 'store'])->middleware('auth','verified')->name('ranking.store');
-
-Route::get('goodies',[GoodieController::class, 'index'])->middleware('auth','verified')->name('goodies.index');
-Route::get('goodies/creer',[GoodieController::class, 'create'])->middleware('auth','verified')->name('goodies.create');
-Route::post('goodies/',[GoodieController::class, 'store'])->middleware('auth','verified')->name('goodies.store');
-Route::get('goodies/{goodie}/editer',[GoodieController::class, 'edit'])->middleware('auth','verified')->name('goodies.edit');
-Route::put('goodies/{goodie}',[GoodieController::class, 'update'])->middleware('auth','verified')->name('goodies.update');
-Route::delete('goodies/{goodie}',[GoodieController::class, 'destroy'])->middleware('auth','verified')->name('goodies.destroy');
-=======
 Route::get('inscription_sessions', [SessiongameUserController::class, 'create'])->middleware('auth')->name('sessiongameusers.create');
 Route::post('inscription_sessions',[SessiongameUserController::class, 'store'])->middleware('auth')->name('sessiongameusers.store');
 Route::post('inscription_sessions/paiement', [SessiongameUserController::class, 'storePayment'])->middleware('auth')->name('sessiongameusers.storePayment');
@@ -143,8 +83,8 @@ Route::delete('posts/{post}',[PostController::class, 'destroy'])->middleware('au
 Route::get('posts/valides',[PostController::class, 'indexValidated'])->middleware('auth')->name('posts.indexValidated');
 Route::get('posts/en-attente',[PostController::class, 'indexPending'])->middleware('auth')->name('posts.indexPending');
 Route::put('posts/{post}',[PostController::class, 'update'])->middleware('auth')->name('posts.update');
-Route::get('posts/en-attente/search',[PostController::class, 'search'])->middleware('auth')->name('posts.search');
-
+Route::get('posts/en-attente/searchPending',[PostController::class, 'searchPending'])->middleware('auth')->name('posts.searchPending');
+Route::get('posts/en-attente/searchValidated',[PostController::class, 'searchValidated'])->middleware('auth')->name('posts.searchValidated');
 
 Route::get('utilisateurs/utilisateurs',[UserController::class, 'indexUsers'])->middleware('auth')->name('users.indexUsers');
 Route::get('utilisateurs/admin-defis',[UserController::class, 'indexAdminChallenge'])->middleware('auth')->name('users.indexAdminChallenge');
@@ -173,4 +113,3 @@ Route::post('goodies/',[GoodieController::class, 'store'])->middleware('auth')->
 Route::get('goodies/{goodie}/editer',[GoodieController::class, 'edit'])->middleware('auth')->name('goodies.edit');
 Route::put('goodies/{goodie}',[GoodieController::class, 'update'])->middleware('auth')->name('goodies.update');
 Route::delete('goodies/{goodie}',[GoodieController::class, 'destroy'])->middleware('auth')->name('goodies.destroy');
->>>>>>> develop
